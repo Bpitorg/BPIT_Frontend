14766f2fb3ac96fa2629a271ed810bdc739cc109
RITIKA <chauhanritika149@gmail.com>
2020-08-25 14:33:13 +0530

    minor

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -2,10 +2,8 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class Configuration{
-  // public url="https://Bpit-backend.herokuapp.com/api"
-  // public server="https://Bpit-backend.herokuapp.com/api/";
-  public url = "http://127.0.0.1:8000/api";
-  public server = "http://127.0.0.1:8000/api/";
+  public url = "http://bpitback.herokuapp.com/api";
+  public server = "http://bpitback.herokuapp.com/api/";
 
     constructor(){}
 }
